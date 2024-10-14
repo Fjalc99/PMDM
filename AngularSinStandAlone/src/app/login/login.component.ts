e05e710d148b77e48c8427ff1959c874c2fdc4da
alcantarilla.cafra24 <alcantarilla.cafra24@triana.salesianos.edu>
2024-10-14 10:53:37 +0200

    Angular

--- AngularSinStandAlone/src/app/login/login.component.ts
+++ AngularSinStandAlone/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  protected readonly value = signal('');
 
+    protected onInput(event: Event) {
+      this.value.set((event.target as HTMLInputElement).value);
+    }
 }
