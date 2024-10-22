dc4ffd6ad77aa6ab7ad00f4b6021cf276c8a9cf4
alcantarilla.cafra24 <alcantarilla.cafra24@triana.salesianos.edu>
2024-10-22 10:39:51 +0200

    Actu

--- start-wars-vehiculo/src/app/app-routing.module.ts
+++ start-wars-vehiculo/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListVehiclesComponent } from './componets/list-vehicles/list-vehicles.component';
+import { PeopleListComponent } from './componets/people-list/people-list.component';
+import { PageNotFoundComponent } from './share/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'vehicles', component: ListVehiclesComponent},
+  {path: 'peoples', component: PeopleListComponent},
+  {path: '', redirectTo: '/vehicles', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- start-wars-vehiculo/src/app/app.module.ts
+++ start-wars-vehiculo/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ListVehiclesComponent } from './componets/list-vehicles/list-vehicles.component';
 import { provideHttpClient } from '@angular/common/http';
+import { MenuComponent } from './share/menu/menu.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { PeopleListComponent } from './componets/people-list/people-list.component';
+import { PageNotFoundComponent } from './share/page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListVehiclesComponent
+    ListVehiclesComponent,
+    PeopleListComponent,
+    MenuComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
