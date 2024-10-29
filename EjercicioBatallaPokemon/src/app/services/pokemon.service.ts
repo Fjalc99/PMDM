8e34bc009f8e3a658eb46d936977f9dd96ff92b1
alcantarilla.cafra24 <alcantarilla.cafra24@triana.salesianos.edu>
2024-10-29 13:27:10 +0100

    BatallaPokemon

--- EjercicioBatallaPokemon/src/app/services/pokemon.service.ts
+++ EjercicioBatallaPokemon/src/app/services/pokemon.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Pokemon, PokemonListResponse } from '../models/pokemon-response.interface';
+import { PokemonDetailResponse} from '../models/pokemon-response.interface';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -13,12 +13,8 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
-  getPokemons(): Observable<PokemonListResponse> {
-    return this.http.get<PokemonListResponse>(`${this.apiUrl}?limit=2`);
-  }
-
-  getPokemon(id: string): Observable<Pokemon> {
-    return this.http.get<Pokemon>(`${this.apiUrl}/${id}`);
+  getPokemon(id: number): Observable<PokemonDetailResponse> {
+    return this.http.get<PokemonDetailResponse>(`${this.apiUrl}/${id}`);
   }
 }
 
